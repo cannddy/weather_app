285a8319adb9ad8622c66cb0a7ebc583601e9f0e
Praveen <itspraveen2004@gmail.com>
2022-11-16 22:41:00 +0530

    .

--- lib/utils/weather_codes.dart
+++ lib/utils/weather_codes.dart
@@ -34,6 +34,69 @@ String convertWeatherCode(int weatherCode) {
     case 57:
       mode = "Dense & intense freezing drizzle";
       break;
+    case 61:
+      mode = "Rain(Slight)";
+      break;
+      case 63:
+      mode = "Rain(Moderate)";
+      break;
+      case 65:
+      mode = "Rain(heavy)";
+      break;
+      case 66:
+      mode = "Freezing Rain(Light)";
+      break;
+      case 67:
+      mode = "Freezing Rain(Heavy)";
+      break;
+      case 71:
+      mode = "Snow fall(Slight)";
+      break;
+      case 73:
+      mode = "Snow fall(Moderate)";
+      break;
+      case 75:
+      mode = "Snow fall(Heavy)";
+      break;
+      case 77:
+      mode = "Snow grains";
+      break;
+      case 80:
+      mode = "Rain showers(Slight)";
+      break;
+      case 81:
+      mode = "Rain showers(Moderate)";
+      break;
+      case 82:
+      mode = "Rain showers(violent)";
+      break;
+      case 85:
+      mode = "Snow showers(Slight)";
+      break;
+      case 86:
+      mode = "Snow showers(Heavy)";
+      break;
+      case 95:
+      mode = "Thunderstorm(Slight or moderate)";
+      break;
+      case 96:
+      mode = "Thunderstorm(slight)";
+      break;
+      case :
+      mode = "Thunderstorm(Heavy hail)";
+      break;
+      case :
+      mode = "()";
+      break;
+      case :
+      mode = "()";
+      break;
+      case :
+      mode = "()";
+      break;
+      case :
+      mode = "()";
+      break;
     default:
   }
   return mode;
